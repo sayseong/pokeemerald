197a9ce06f6d5f2e56d59b3166caefd36e98c1ab
none <none>
2020-12-17 18:06:01 +0800

    编译选项修改。

--- makedebug.sh
+++ makedebug.sh
@@ -1,13 +1,12 @@
 if [ "$1" == "fast" ]; then
   #rm build/emerald/src/pokemon.o
-  make DINFO=1 MODERN=1 -f fast.mk elf
+  make DINFO=1 -f fast.mk elf
 elif [ "$1" == "fastn" ]; then
-  make DINFO=1 MODERN=1 NODEP=0 -f fast.mk elf
+  make DINFO=1 NODEP=0 -f fast.mk elf
 elif [ "$1" == "full" ]; then
-  make DINFO=1 -j8 MODERN=1
+  make -j8 DINFO=1
 elif [ "$1" == "final" ]; then
-  rm -rf build
-  make -j8
+  make -j8 MODERN=1
 elif [ "$1" == "fastrun" ]; then
   make DINFO=1 -f fast.mk elf
   mgba --gdb pokeemerald.gba &
