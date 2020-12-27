3ff2dd2ef7481436153613ea6e917c7ca8688edb
none <none>
2020-12-27 12:35:19 +0800

    修改初始精灵

--- src/starter_choose.c
+++ src/starter_choose.c
@@ -118,9 +118,9 @@ static const u8 sStarterLabelCoords[STARTER_MON_COUNT][2] =
 
 static const u16 sStarterMon[STARTER_MON_COUNT] =
 {
-    SPECIES_TREECKO,
-    SPECIES_TORCHIC,
-    SPECIES_MUDKIP,
+    SPECIES_GROOKEY,
+    SPECIES_SCORBUNNY,
+    SPECIES_SOBBLE,
 };
 
 static const struct BgTemplate sBgTemplates[3] =
